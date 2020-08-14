6d97e4ed29cb028900f8d56a392ead08c11eb95b
Minh Tri Tran <ttran@hu.ibm.com@Minhs-MacBook-Pro.local>
2020-08-14 18:09:04 +0200

    feat: rev string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   let reverseStr ='';
+//   for( let i = str.length -1 ; i >=0 ; i-- ) {
+//     reverseStr += str[i];
+//   }
+//   return reverseStr;
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//   for(let character of str) reversed = character + reversed;
+//   return reversed;
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+//   }
+
+function reverse(str) {
+  return str.split('').reduce( (rev, char) => { return char + rev}, '' );
+ 
+}
 
 module.exports = reverse;
